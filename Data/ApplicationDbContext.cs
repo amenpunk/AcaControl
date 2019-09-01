8e8ed32f20cf5573b8c796b05e6cd9acd1569ede
amenpunk <ondasycircuitos@gmail.com>
2019-09-01 15:50:25 -0600

    start config :smile

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Sistema.Models;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Sistema.Models.Categoria> Categoria { get; set; }
+    }
